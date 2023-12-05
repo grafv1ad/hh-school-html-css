9f721e6d66f6876b42a46c35398053deaa0a1178
Vladislav Petrov <grafv1ad@yandex.com>
2023-12-05 18:11:22 +0500

    Updates

---
+++ index.72be8890.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 3000,
+            pauseOnMouseEnter: true
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            hideOnClick: false,
+            bulletClass: "pagination__bullet",
+            bulletActiveClass: "pagination__bullet_active"
+        }
+    });
+});
+
+//# sourceMappingURL=index.72be8890.js.map
