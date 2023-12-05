3207a5b9ad9efd99e9eb892bbdce412dcec2a1f1
Vladislav Petrov <grafv1ad@yandex.com>
2023-12-05 17:12:06 +0500

    fix swiper.js
    Parcel ни в какую не хотел дружить с локальным файлом свайпера, подключил через cdn

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    speed: 500,
-    autoplay: {
-        delay: 3000,
-        pauseOnMouseEnter: true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        hideOnClick: false,
-        bulletClass: 'pagination__bullet',
-        bulletActiveClass: 'pagination__bullet_active',
-    },
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        speed: 500,
+        autoplay: {
+            delay: 3000,
+            pauseOnMouseEnter: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            hideOnClick: false,
+            bulletClass: 'pagination__bullet',
+            bulletActiveClass: 'pagination__bullet_active',
+        },
+    });
 });
