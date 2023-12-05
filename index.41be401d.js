d392055a89ac90c8a1bece8e9beb7908de1a2449
Vladislav Petrov <grafv1ad@yandex.com>
2023-12-05 17:11:12 +0500

    Updates

---
+++ index.41be401d.js
@@ -0,0 +1,3 @@
+new Swiper(".swiper",{loop:!0,speed:500,autoplay:{delay:3e3,pauseOnMouseEnter:!0},pagination:{el:".swiper-pagination",clickable:!0,hideOnClick:!1,bulletClass:"pagination__bullet",bulletActiveClass:"pagination__bullet_active"}});//# sourceMappingURL=index.41be401d.js.map
+
+//# sourceMappingURL=index.41be401d.js.map
